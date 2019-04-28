4c0fad30073f058bbbd84cc2b35a9f993f911ef9
Henrique Omena <88lmao@gmail.com>
2019-04-28 20:23:26 +0100

    Created components and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//import HelloWorld from '@/components/HelloWorld'
+import Home from '@/componnets/Homne'
+import Login from '@/componnets/Homne'
+import About from '@/componnets/about'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+   /*  {
       path: '/',
       name: 'HelloWorld',
+      component: HelloWorld
+    }, */
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '*',
+      redirect: '/',
       component: HelloWorld
     }
   ]
